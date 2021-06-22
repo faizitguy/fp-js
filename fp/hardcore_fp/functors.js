595e75344cc6cdf11dcfa3d7561be3fd9027ed52
faizitguy <faizkhan3597@gmail.com>
2021-06-22 17:01:20 +0530

    functors

--- fp/hardcore_fp/functors.js
+++ fp/hardcore_fp/functors.js
@@ -1,19 +1,19 @@
-const Box = (x) => ({
-  map: (f) => Box(f(x)),
-});
+// const Box = (x) => ({
+//   map: (f) => Box(f(x)),
+// });
 
-const nextCharForNumberString = (str) => {
-  const trimmed = str.trim();
-  const number = parseInt(trimmed);
-  const nextNumber = Number(number + 1);
-  return String.fromCharCode(nextNumber);
-};
+// const nextCharForNumberString = (str) => {
+//   const trimmed = str.trim();
+//   const number = parseInt(trimmed);
+//   const nextNumber = Number(number + 1);
+//   return String.fromCharCode(nextNumber);
+// };
 
-// const result = nextCharForNumberString(" 65 ");
+// // const result = nextCharForNumberString(" 65 ");
 
-const result = () =>
-  ["a"].map((x) => x.toUpperCase()).map((x) => String.fromCharCode(x));
+// const result = () =>
+//   ["a"].map((x) => x.toUpperCase()).map((x) => String.fromCharCode(x));
 
-console.log(result);
+// console.log(result);
 
-// things aren't so dot chainable out of the box
+// // things aren't so dot chainable out of the box
