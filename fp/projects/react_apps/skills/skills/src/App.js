92485293e70a761074a0d5bfe53a39f8fc2bfb34
faizitguy <faizkhan3597@gmail.com>
2021-06-23 15:32:28 +0530

    dynamic form created

--- fp/projects/react_apps/skills/skills/src/App.js
+++ fp/projects/react_apps/skills/skills/src/App.js
@@ -1,22 +1,40 @@
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import Form from "./Components/Form/Form";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Skills App</h1>
-      <Form
-        className="form"
-        title="Form"
-        model={[
-          { key: "skill name", label: "Skill Name", props: { required: true } },
-          { key: "rating", label: "Rating", type: "number" },
-        ]}
-        onSubmit={(model) => this.onSubmit(model)}
-      />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: [{ id: 1, skill: "react", rating: "4.5" }],
+  };
+
+  onSubmit = (model) => {
+    model.id = +new Date();
+    this.setState({
+      data: [model, ...this.state.data],
+    });
+  };
+  render() {
+    console.log(this.state.data);
+    return (
+      <div className="App">
+        <h1> Skills App</h1>
+        <Form
+          className="form"
+          title="Form"
+          model={[
+            {
+              key: "skill",
+              label: "Skill Name",
+              props: { required: true },
+            },
+            { key: "rating", label: "Rating", type: "number" },
+          ]}
+          onSubmit={(model) => this.onSubmit(model)}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
