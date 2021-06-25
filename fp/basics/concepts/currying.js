612dfe0817f016c0059f870dce4904eced6c9af2
faizitguy <faizkhan3597@gmail.com>
2021-06-25 11:41:27 +0530

    presentation

--- fp/basics/concepts/currying.js
+++ fp/basics/concepts/currying.js
@@ -40,35 +40,57 @@ const hfun = function (f, g, h) {
   return f + g + h;
 };
 
-/*const curriedF = curry(ffun);
-const curriedG = curry(gfun);
-const curriedH = curry(hfun);
+// check how many words
 
-const newFun = pipe (
-    curriedF(1)(2),
-    curriedG(4),
-    curriedH(5)(6));*/
+function checkWordsLength(str) {
+  let str1 = str.trim();
+  let str2 = str1.split(" ");
+  let len = str2.length;
+  return len;
+}
 
-const newFun = pipe(curry(ffun)(1)(2), curry(gfun)(4), curry(hfun)(5)(6));
+const trim = (str) => str.trim();
 
-console.log(newFun(1));
+const split = (str) => str.split(" ");
 
-const doubleNum = function (num) {
-  return num + num;
-};
+const check = (arr) => arr.length;
 
-const totalIt = function (n1, n2, n3, n4) {
-  return n1 + n2 + n3 + n4;
-};
+// console.log(checkWordsLength("Hi I'm faiz ahmed khan"));
 
-const doArray = function (num1, num2) {
-  return [num1, num2];
-};
+// console.log(check(split(trim("Hi I'm faiz"))));
 
-const newFunction = pipe(
-  doubleNum,
-  curry(totalIt)(3)(2)(1),
-  curry(doArray)(50)
-);
+const checkIt = pipe(trim, split, check);
 
-// console.log(newFunction(10));
+console.log(checkIt("Hi I'm faiz"));
+// /*const curriedF = curry(ffun);
+// const curriedG = curry(gfun);
+// const curriedH = curry(hfun);
+
+// const newFun = pipe (
+//     curriedF(1)(2),
+//     curriedG(4),
+//     curriedH(5)(6));*/
+
+// const newFun = pipe(curry(ffun)(1)(2), curry(gfun)(4), curry(hfun)(5)(6));
+
+// console.log(newFun(1));
+
+// const doubleNum = function (num) {
+//   return num + num;
+// };
+
+// const totalIt = function (n1, n2, n3, n4) {
+//   return n1 + n2 + n3 + n4;
+// };
+
+// const doArray = function (num1, num2) {
+//   return [num1, num2];
+// };
+
+// const newFunction = pipe(
+//   doubleNum,
+//   curry(totalIt)(3)(2)(1),
+//   curry(doArray)(50)
+// );
+
+// // console.log(newFunction(10));
