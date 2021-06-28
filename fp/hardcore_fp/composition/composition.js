f80a8fd50bcabfd4df2745ff62a4f89c6b14aef3
faizitguy <faizkhan3597@gmail.com>
2021-06-28 14:59:10 +0530

    composition assignment

--- fp/hardcore_fp/composition/composition.js
+++ fp/hardcore_fp/composition/composition.js
@@ -1,5 +1,6 @@
-// import { curry } from "ramda";
+// import { curry, compose, replace } from "ramda";
 
+const { curry, compose, replace } = require("ramda");
 // const add = (x, y) => x + y;
 
 // const toUpper = (str) => str.toUpperCase();
@@ -13,3 +14,15 @@
 // const shout = compose(exclaim, toUpper);
 
 // console.log(shout("tears"));
+
+const _underscore = replace(/\W+/g, "_"); //<-- leave this alone and use to sanitize
+
+const _samllCase = (str) => str.toLowerCase();
+
+const join = (str) => str.join("");
+
+const sanitizeNames = compose(_samllCase, _underscore);
+
+const result = sanitizeNames("Faiz Ahmed Khan");
+
+console.log(result);
