0f59835db1219fa329e00a5dac987f8ffa27a352
faizitguy <faizkhan3597@gmail.com>
2021-07-12 11:05:24 +0530

    blog app

--- fp/functional_architecture/index.js
+++ fp/functional_architecture/index.js
@@ -1,10 +1,10 @@
 // cli blog
-
-const { Task } = require("./lib/types");
 const { save, all } = require("./lib/db");
-const { last } = require("ramda");
+const { Task, Id } = require("./lib/types");
+const { Free, liftF } = require("./lib/free");
+const { Pure } = Free;
 const { taggedSum } = require("daggy");
-const { liftF } = require("./lib/free");
+const { last } = require("ramda");
 
 const Console = taggedSum("Console", { Question: ["q"], Print: ["s"] });
 const Db = taggedSum("Db", {
@@ -12,49 +12,46 @@ const Db = taggedSum("Db", {
   All: ["table", "query"],
 });
 
-const print = (s) => liftF(Console.Print(s));
-const question = (s) => liftF(Console.Question(s));
-
 const AuthorTable = "Authors";
-const Author = (name) => ({ name });
-
 const PostTable = "Post";
+
+const Author = (name) => ({ name });
 const Post = (title, body) => ({ title, body });
 
-const dbAll = (table, query) => liftF(Db.All(table, query));
-const dbSave = (table, record) => liftF(Db.Save(table, record));
-
+// Thanks flavio!
 const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
 const writeOutput = (s) => Task((_rej, res) => res(console.log(s)));
-
 const getInput = (q) =>
   Task((rej, res) => readline.question(q, (i) => res(i.trim())));
 
-const menu = () =>
-  getInput(
-    "navigation menu choose one  (createAuthor, write, latest, all)"
-  ).map((route) => router[route]);
+const formatPost = (post) => `${post.title}:\n${post.body}`;
 
-const formatPost = (post) => `${post.title} :\n ${post.body}`;
-const print = (s) => Task((_rej, res) => res(console.log(s)));
+const print = (s) => liftF(Console.Print(s));
+const question = (s) => liftF(Console.Question(s));
+
+const dbAll = (table, query) => liftF(Db.All(table, query));
+const dbSave = (table, record) => liftF(Db.Save(table, record));
+
+const questions = {
+  whereTo:
+    "Where do you want to go today? (createAuthor, write, latest, all, end)",
+  title: "Title? ",
+  body: "Body? ",
+  name: "Name? ",
+};
+
+const menu = () => question(questions.whereTo).map((route) => router[route]());
 
 const latest = () =>
-  all(PostTable)
-    .chain((posts) => last(posts))
+  dbAll(PostTable)
+    .map((posts) => last(posts))
     .map(formatPost)
     .chain(print)
     .map(() => menu());
-
-const write = () =>
-  getInput("Title : ")
-    .chain((title) => getInput("Body : "))
-    .map((body) => Post(title, body))
-    .chain((post) => save(PostTable, post))
-    .map(() => latest());
 
 // we're returning a task that holds another function which returns another task as vice verca
 // recurrsive operation is happening here
@@ -62,17 +59,66 @@ const write = () =>
 // () => Task () => Task => ...
 // () => Task Task Task ...
 
+const write = () =>
+  question(questions.title)
+    .chain((title) => question(questions.body).map((body) => Post(title, body)))
+    .chain((post) => dbSave(PostTable, post))
+    .map(() => latest());
+
 const createAuthor = () =>
-  getInput("Name?")
+  question(questions.name)
     .map((name) => Author(name))
-    .chain((author) => save(AuthorTable, author))
+    .chain((author) => dbSave(AuthorTable, author))
     .map(() => menu());
 
 const start = () =>
-  all(AuthorTable).map((authors) => (authors.length ? menu() : createAuthor()));
+  dbAll(AuthorTable).map((authors) =>
+    authors.length ? menu() : createAuthor()
+  );
 
-const router = { menu, createAuthor, write, latest };
+const end = () => Pure(Console.Print("fin"));
 
-const runApp = (f) => f().fork(console.error, console.log);
+const router = { menu, createAuthor, write, latest, end };
 
-runApp(start);
+const answers = {
+  [questions.whereTo]: "end",
+  [questions.title]: "A title",
+  [questions.body]: "A body",
+  [questions.name]: "Some name",
+};
+
+const dbToTask = (x) => x.cata({ Save: save, All: all });
+
+const consoleToTask = (x) => x.cata({ Question: getInput, Print: writeOutput });
+
+const consoleToId = (x) =>
+  x.cata({
+    Question: (q) => Id.of(answers[q]),
+    Print: (s) => Id.of(`writing the string ${s}`),
+  });
+
+const dbToId = (x) =>
+  x.cata({
+    Save: (table, r) => Id.of(`saving to ${r} ${table}`),
+    All: (table, q) => Id.of([]),
+  });
+
+const interpretTest = (free) =>
+  Task.of(
+    free.foldMap((x) => (x.table ? dbToId(x) : consoleToId(x)), Id.of).extract()
+  );
+
+const interpretReal = (free) =>
+  free.foldMap((x) => (x.table ? dbToTask(x) : consoleToTask(x)), Task.of);
+
+const runApp = (free, interpreter, log = []) =>
+  free.cata({
+    Pure: (x) => log.concat([x]),
+    Impure: (x) =>
+      interpreter(free).fork(console.error, (g) =>
+        runApp(g, interpreter, log.concat([x]))
+      ),
+  });
+
+const result = runApp(start(), interpretTest);
+console.log(result);
